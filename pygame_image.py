5709245ac821a8e08429aa031e2cab17a8812a59
Koyo.N <c0a22112c4@edu.teu.ac.jp>
2023-06-13 17:14:17 +0900

    練習６

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,28 @@ def main():
     kk_img = pg.image.load("ex01/fig/3.png")            #コウカトン画像読み込み
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_imgs = pg.transform.rotozoom(kk_img, 10, 1.0)
+    bg_imgs = [bg_img,pg.transform.flip(bg_img, True, False)]*2
+    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            
+        x = tmr % 3200
+        screen.blit(bg_img, [x , 0])
         
-        screen.blit(bg_img, [0, 0])
+        for i in range(4):
+            screen.blit(bg_imgs[i], [1600*i-x, 0])
+        
         if tmr % 2 == 1:
             screen.blit(kk_imgs, [300, 200])
         else:
             screen.blit(kk_img, [300, 200])
+        
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
+
 
 
 if __name__ == "__main__":
